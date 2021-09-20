f7516b767c54dfdd57094d9586c5cee9fe982646
haya14busa <haya14busa@gmail.com>
2021-09-20 16:27:54 +0000

    implement the action

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 set -e
 
+if [ -n "${GITHUB_WORKSPACE}" ] ; then
+  cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
+fi
+
 echo '::group::🐶 Installing shfmt ... https://github.com/mvdan/sh'
 TEMP_PATH="$(mktemp -d)"
 $GITHUB_ACTION_PATH/install_shfmt.sh -b "${TEMP_PATH}"
 echo '::endgroup::'
-echo "${TEMP_PATH}" >>"${GITHUB_PATH}"
+
+# shellcheck disable=SC2086
+"${TEMP_PATH}/shfmt" ${INPUT_SHFMT_FLAGS} -w .
